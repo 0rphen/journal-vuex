3741b4fd4274b8aee3fb7aba9f94f47bd3d5013d
0rphen <the.bebop.88@gmail.com>
2021-08-11 23:57:21 -0500

    first commit

---
+++ src/modules/daybook/store/journal/mutations.js
@@ -0,0 +1,15 @@
+// export const myMutation = ( state ) => {
+
+// }
+
+export const setEntries = ( /*state*/) => {
+
+}
+
+export const updateEntrie = ( /*state*/) => {
+
+}
+
+export const addEntrie = ( /*state*/) => {
+
+}
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,29 @@
+export default () => ({
+  isLoading: false,
+  entries: [
+    {
+      id: new Date().getTime(),
+      date: new Date().toDateString(),
+      text: 'Aliquam erat volutpat.  Nunc eleifend leo vitae magna.  In id erat non orci commodo lobortis.  Proin neque massa, cursus ut, gravida ut, lobortis eget, lacus.  Sed diam.  Praesent fermentum tempor tellus.  Nullam tempus.  Mauris ac felis vel velit tristique imperdiet.  Donec at pede.  Etiam vel neque nec dui dignissim bibendum.  Vivamus id enim.  Phasellus neque orci, porta a, aliquet quis, semper a, massa.  Phasellus purus.  Pellentesque tristique imperdiet tortor.  Nam euismod tellus id erat.',
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 10,
+      date: new Date().toDateString(),
+      text: 'Aliquam erat volutpat.  Nunc eleifend leo vitae magna.  In id erat non orci commodo lobortis.  Proin neque massa, cursus ut, gravida ut, lobortis eget, lacus.  Sed diam.  Praesent fermentum tempor tellus.  Nullam tempus.  Mauris ac felis vel velit tristique imperdiet.  Donec at pede.  Etiam vel neque nec dui dignissim bibendum.  Vivamus id enim.  Phasellus neque orci, porta a, aliquet quis, semper a, massa.  Phasellus purus.  Pellentesque tristique imperdiet tortor.  Nam euismod tellus id erat.',
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 20,
+      date: new Date().toDateString(),
+      text: 'Aliquam erat volutpat.  Nunc eleifend leo vitae magna.  In id erat non orci commodo lobortis.  Proin neque massa, cursus ut, gravida ut, lobortis eget, lacus.  Sed diam.  Praesent fermentum tempor tellus.  Nullam tempus.  Mauris ac felis vel velit tristique imperdiet.  Donec at pede.  Etiam vel neque nec dui dignissim bibendum.  Vivamus id enim.  Phasellus neque orci, porta a, aliquet quis, semper a, massa.  Phasellus purus.  Pellentesque tristique imperdiet tortor.  Nam euismod tellus id erat.',
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 39,
+      date: new Date().toDateString(),
+      text: 'Aliquam erat volutpat.  Nunc eleifend leo vitae magna.  In id erat non orci commodo lobortis.  Proin neque massa, cursus ut, gravida ut, lobortis eget, lacus.  Sed diam.  Praesent fermentum tempor tellus.  Nullam tempus.  Mauris ac felis vel velit tristique imperdiet.  Donec at pede.  Etiam vel neque nec dui dignissim bibendum.  Vivamus id enim.  Phasellus neque orci, porta a, aliquet quis, semper a, massa.  Phasellus purus.  Pellentesque tristique imperdiet tortor.  Nam euismod tellus id erat.',
+      picture: null,
+    },
+  ]
+})
